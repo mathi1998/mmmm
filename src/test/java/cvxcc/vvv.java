56b76229ca676edd7838d7c6dd437d97926752ec
training_d2.03.07 <training_d2.03.07@ddc2-d-9r84t72.dir.svc.accenture.com>
2019-07-05 15:25:00 +0530

    nnnn

---
+++ src/test/java/cvxcc/vvv.java
@@ -0,0 +1,16 @@
+package cvxcc;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+(
+		plugin= {"json:cucumber.jason"}
+		)
+
+
+public class vvv {
+
+}
